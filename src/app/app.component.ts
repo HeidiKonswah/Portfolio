060723264f6af6053b616320765d00f01d439a77
heidi konswah <heidikonswah@gmail.com>
2020-05-13 09:17:48 +0000

    style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'heidi-port';
+  burgerMenu = false;
+  toggleMenu(): void{
+    this.burgerMenu = !this.burgerMenu;
+    console.log(this.burgerMenu);
+  }
 }
